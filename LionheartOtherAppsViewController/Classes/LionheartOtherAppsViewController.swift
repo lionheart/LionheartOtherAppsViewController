95bc0fd527c4d139b5887ebedc7f067de3847053
Dan Loewenherz <dan@lionheartsw.com>
2017-08-31 18:11:55 -0500

    remove unused files

--- LionheartOtherAppsViewController/Classes/LionheartOtherAppsViewController.swift
+++ LionheartOtherAppsViewController/Classes/LionheartOtherAppsViewController.swift
@@ -1,8 +1,17 @@
 //
-//  LionheartOtherAppsViewController.swift
-//  Pods
+//  Copyright 2017 Lionheart Software LLC
 //
-//  Created by Daniel Loewenherz on 8/31/17.
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//  http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
 //
 //
 
@@ -69,7 +78,7 @@ extension LionheartOtherAppsViewController: UITableViewDelegate {
         tableView.deselectRow(at: indexPath, animated: true)
 
         guard let urlString = apps[indexPath.row]["trackViewUrl"],
-            let url = URL(string: urlString) {
+            let url = URL(string: urlString) else {
                 return
         }
 
